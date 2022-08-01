d61caabbf8efccbb2ab6e04ddd00b4fc91c268db
RayBar72 <3963@holbertonschool.com>
2022-08-01 20:35:20 -0300

    First version. Be aware that looks to be an error in web example

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Method that determines if all the boxes can be opened'''
+
+
+def canUnlockAll(boxes):
+    '''Returns True if all boxes can be unlocked'''
+    for i in range(len(boxes) - 1):
+        c = i + 1
+        if c in boxes[i]:
+            pass
+            print('c es {}'.format(c))
+        else:
+            print('c es {}'.format(c))
+            return False
+    return True
