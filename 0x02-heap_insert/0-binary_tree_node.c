a5cc7747f5db27b5fc53d87fd4d8faccd33480da
RayBar72 <3963@holbertonschool.com>
2022-08-17 20:51:26 -0300

    Backup task 0

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,6 +1,11 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_node - function that creates a node of binary tree
+ * @parent: pointer to parent node
+ * @value: value to be in node
+ * Return: pointer to new node o NULL
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -9,6 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	son = malloc(sizeof(*son));
 	if (!son)
 		return (NULL);
+	memset(son, 0, sizeof(*son));
 	son->parent = parent;
 	son->n = value;
 	return (son);
