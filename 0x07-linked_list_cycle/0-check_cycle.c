520a352f1bb466b05980a74998215c1cc8c0a375
RayBar72 <3963@holbertonschool.com>
2022-09-20 19:11:32 -0300

    Backup 220920h1911

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function that check if a sll has a cycle
+ * @list: pointer to sll
+ * Retur - 1 if is a cycle, 0 other wise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *non = list, *par = list;
+
+	if (!list)
+		return (0);
+
+	while (par->next && par->next->next)
+	{
+		non = non->next;
+		par = par->next->next;
+		if (non == par)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
