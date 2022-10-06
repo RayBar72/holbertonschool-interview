ec7baec4e8081535d3981333e62f870bc730988b
RayBar72 <3963@holbertonschool.com>
2022-10-06 17:50:17 -0300

    Ussing bitwize op

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,59 +9,17 @@ def validUTF8(data):
     '''
     Function that determines if a given data set represents a valid UTF-8
     '''
-    if type(data) is not list:
-        return False
-    for i in data:
-        if type(i) is not int:
-            return False
-        if i < 0:
-            return False
-        if i > 247:
-            return False
-        if i is None:
-            return False
-        if i is False:
-            return False
-        if len(data) == 0:
-            return False
-
-    data = [str(bin(x)).split('b')[1] for x in data]
-
-    for i in range(len(data)):
-        if len(data[i]) < 8:
-            data[i] = ('0' * (8 - len(data[i]))) + data[i]
-        elif len(data[i]) > 8:
-            return False
-
-    for i in range(len(data)):
-        if data[i][:5] == '11110':
-            try:
-                if not (data[i + 1] or data[i + 2] or data[i + 3]):
-                    return False
-                if data[i + 1] != data[i + 2] != data[i + 3] != '10':
-                    return False
-            except Exception:
+    b = 0
+    for n in data:
+        byte = n & 0xFF
+        if b:
+            if byte >> 6 != 2:
                 return False
-
-        elif data[i][:4] == '1110':
-            try:
-                if not (data[i + 1] or data[i + 2]):
-                    return False
-                if data[i + 1] != data[i + 2] != '10':
-                    return False
-            except Exception:
-                return False
-
-        elif data[i][:3] == '110':
-            try:
-                if not (data[i + 1]):
-                    return False
-                if data[i + 1] != '10':
-                    return False
-            except Exception:
-                return False
-
-        elif data[i][0] == '1':
+            b -= 1
+            continue
+        while (1 << abs(7 - b)) & byte:
+            b += 1
+        if b == 1 or b > 4:
             return False
-
-    return True
+        b = max(b - 1, 0)
+    return b == 0
