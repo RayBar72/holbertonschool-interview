09f96ba531a2605a4667168f48b0025f01e32706
RayBar72 <3963@holbertonschool.com>
2022-10-11 18:16:36 -0300

    Backup 221011h1816

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,104 @@
+#include "slide_line.h"
+
+/**
+ * zero_move - function that moves zeros over the int array
+ * @line: array to be orderer
+ * @size: size of array line
+ * @direction: left or right side to be changed numbers
+ */
+
+static void zero_move(int *line, size_t size, int direction)
+{
+size_t i = 0;
+
+	if (direction == SLIDE_LEFT)
+	{
+		while (i < size - 1)
+		{
+			if (line[i] == 0 && line[i + 1] !=0)
+			{
+				line[i] = line[i + 1];
+				line[i + 1] = 0;
+			}
+			i++;
+		}
+	}
+	else
+	{
+		i = size - 1;
+		while (i)
+		{
+			if (line[i] == 0 && line[i - 1] != 0)
+			{
+				line[i] = line[i - 1];
+				line[i - 1] = 0;
+			}
+			i--;
+		}
+	}
+}
+
+/**
+ * sum_game - function that moves zeros over the int array
+ * @line: array to be orderer
+ * @size: size of array line
+ * @direction: left or right side to be changed numbers
+ */
+
+static void sum_game(int *line, size_t size, int direction)
+{
+size_t i = 0;
+int a = 0;
+
+	if (direction == SLIDE_LEFT)
+	{
+		while (i < size - 1)
+		{
+			if (line[i] == line[i + 1])
+			{
+				a = line[i];
+				line[i] = line[i + 1] + a;
+				line[i + 1] = 0;
+			}
+			i++;
+		}
+	}
+	else
+	{
+		i = size - 1;
+		while (i)
+		{
+			if (line[i] ==line[i - 1])
+			{
+				a = line[i];
+				line[i] = line[i - 1] + a;
+				line[i - 1] = 0;
+			}
+			i--;
+		}
+	}
+}
+
+/**
+ * slide_line - function that moves zeros over the int array
+ * @line: array to be orderer
+ * @size: size of array line
+ * @direction: left or right side to be changed numbers
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t i = 0;
+
+	if (direction == SLIDE_LEFT || direction == SLIDE_RIGHT)
+	{
+		for (i = 0; i < size; i++)
+			zero_move(line, size, direction);
+		sum_game(line, size, direction);
+		for (i = 0; i < size; i++)
+			zero_move(line, size, direction);
+		return (1);
+	}
+	else
+		return (0);
+}
