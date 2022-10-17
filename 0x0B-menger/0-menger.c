09c0e42fe96ef21b9ae7d382437cf6b8cb24f969
RayBar72 <3963@holbertonschool.com>
2022-10-17 17:28:09 -0300

    Backup 221017h15828

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,12 +1,40 @@
 #include "menger.h"
 
+void menger_1(int level)
+{
+	int i = 0, j = 0;
 
+	for (i = 0; i <= level; i++)
+	{	
+		for (j = 0; j <= level; j++)
+		{
+			printf("###");
+		}
+		putchar(10);
+		for (j = 0; j <= level; j++)
+		{
+			printf("# #");
+		}
+		putchar(10);
+		for (j = 0; j <= level; j++)
+		{
+			printf("###");
+		}
+		putchar(10);
+	}
+}
 
 void menger(int level)
 {
-	int counter = 0;
+	double counter = 0;
 
-		if (level < 0)
-		exit;
-	
+	if (level < 0)
+		exit(0);
+	else if (level == 0)
+		printf("#\n");
+	else if (level >= 1)
+	{
+		counter = pow(3, (level - 1));
+		menger_1(counter);
+	}
 }
