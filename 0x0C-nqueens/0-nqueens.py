49e073697f4dffff0df5c2c15f6988a0c944e2bb
RayBar72 <3963@holbertonschool.com>
2022-11-03 20:15:52 -0300

    Commenting arg 221103h2016

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -107,15 +107,15 @@ if __name__ == '__main__':
 
     n = sys.argv[1]
 
-    if n.isdigit() is False:
-        print("N must be a number")
-        sys.exit(1)
+    # if n.isdigit() is False:
+    #     print("N must be a number")
+    #     sys.exit(1)
 
     n = int(n)
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    # if n < 4:
+    #     print("N must be at least 4")
+    #     sys.exit(1)
 
     for i in range(n):
         x = coordenadas(n, i)
