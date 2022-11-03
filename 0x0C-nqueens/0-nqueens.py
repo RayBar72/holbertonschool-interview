478c4ddacd93128c20b41a317e01fdb2f4abd6ad
RayBar72 <3963@holbertonschool.com>
2022-11-03 20:53:56 -0300

    Test 221103h2036

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,123 +1,90 @@
 #!/usr/bin/python3
-'''Modulus that solves N-queens problem'''
-import sys
+"""
+N-queens
+"""
 
 
-def busqueda(n, q, maldita):
-    '''Function that searches in a given board'''
-    never = []
-    lista = [x for x in range(q, n * n)]
-    matrix = []
-    y = 0
-    for i in range(n):
-        renglon = []
-        for j in range(n):
-            renglon.append(y)
-            y += 1
-        matrix.append(renglon)
-    renglon = q // n
-    columna = q % n
+class QueenChessBoard:
+    def __init__(self, size):
+        self.size = size
+        self.columns = []
 
-    row = [x for x in lista if x < (n * (q // n + 1))]
+    def place_in_next_row(self, column):
+        self.columns.append(column)
 
-    col = [x for x in lista if x in [x for x in range(q, n * n, n)]]
+    def remove_in_current_row(self):
+        return self.columns.pop()
 
-    d_menos = []
-    for i in range(n):
-        for j in range(n):
-            if i - j == renglon - columna:
-                d_menos.append(matrix[i][j])
+    def is_this_column_safe_in_next_row(self, column):
+        row = len(self.columns)
+        for queen_column in self.columns:
+            if column == queen_column:
+                return False
 
-    d_mas = []
-    for i in range(n):
-        for j in range(n):
-            if i + j == renglon + columna:
-                d_mas.append(matrix[i][j])
+        for queen_row, queen_column in enumerate(self.columns):
+            if queen_column - queen_row == column - row:
+                return False
 
-    maldita = row + col + d_menos + d_mas + maldita
-    maldita = list(set(maldita))
-    for i in maldita:
-        never.append(i)
+        for queen_row, queen_column in enumerate(self.columns):
+            if ((self.size - queen_column) - queen_row ==
+                    (self.size - column) - row):
+                return False
 
-    posible = [x for x in lista if x not in maldita if x not in never]
-    ahora = [x for x in posible if x in matrix[renglon + 1]]
-    papa = [q]
-    return [len(ahora)], ahora, papa, maldita
+        return True
+
+    def display(self):
+        lista = []
+        for row in range(self.size):
+            for column in range(self.size):
+                if column == self.columns[row]:
+                    lista.append([row, column])
+        print(lista, end='')
 
 
-def listo(n, listar):
-    '''
-    Function that takes care different posibilities for
-    queen initial position in the board
-    '''
-    retornar = []
-    lento = []
-    for i, l in enumerate(listar[0]):
-        pos = i * 3 + 1
-        for j, p in enumerate(listar[pos]):
-            x = busqueda(n, p, listar[pos + 2])
-            if x[0][0] != 0:
-                lento = lento + x[0]
-                retornar.append(x[1])
-                retornar.append(listar[pos + 1] + x[2])
-                retornar.append(x[3])
+def solve_queen(size):
+    board = QueenChessBoard(size)
+    number_of_solutions = 0
+    row = 0
+    column = 0
+
+    while True:
+        while column < size:
+            if board.is_this_column_safe_in_next_row(column):
+                board.place_in_next_row(column)
+                row += 1
+                column = 0
+                break
             else:
-                pass
-    retornar.insert(0, lento)
-    return tuple(retornar)
+                column += 1
+
+        if (column == size or row == size):
+            if row == size:
+                board.display()
+                print()
+                number_of_solutions += 1
+                board.remove_in_current_row()
+                row -= 1
+
+            try:
+                prev_column = board.remove_in_current_row()
+            except IndexError:
+                break
+
+            row -= 1
+            column = 1 + prev_column
 
 
-def coordenadas(n, q):
-    x = busqueda(n, q, [])
-    queentionary = {}
-    for i in range(n - 1):
-        try:
-            lento = x[0]
-            lista = list(x)
-            queentionary[i + 2] = lista
-            x = listo(n, lista)
-        except Exception as e:
-            break
-    if queentionary.get(n) is not None:
-        queen = queentionary[n]
-        reinitas = []
-        for i, l in enumerate(queen[0]):
-            pos = i * 3 + 1
-            for j, p in enumerate(queen[pos]):
-                reinitas.append(queen[pos + 1] + [p])
-        coordenadas = []
-        for row in reinitas:
-            for r in row:
-                cero = r // n
-                uno = r % n
-                coordenadas.append([cero, uno])
-        return coordenadas
-    else:
-        return None
-
-
-# def myqueens(n):
-if __name__ == '__main__':
-    '''Main function that calculates queens challange'''
+if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    n = sys.argv[1]
-
-    if n.isdigit() is False:
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
-
-    n = int(n)
-
-    if n < 4:
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    for i in range(n):
-        x = coordenadas(n, i)
-        if x is not None and x != []:
-            print('{}'.format(str(x)))
+    solve_queen(int(sys.argv[1]))
