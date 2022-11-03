c84502d3077e7fedc86bbbb0e2eeb822efd4d129
RayBar72 <3963@holbertonschool.com>
2022-11-03 20:25:18 -0300

    Checking impresion output 221103h2016

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -101,23 +101,24 @@ if __name__ == '__main__':
     '''Main function that calculates queens challange'''
     import sys
 
-    # if len(sys.argv) != 2:
-    #     print("Usage: nqueens N")
-    #     sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
     n = sys.argv[1]
 
-    # if n.isdigit() is False:
-    #     print("N must be a number")
-    #     sys.exit(1)
+    if n.isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
 
     n = int(n)
 
-    # if n < 4:
-    #     print("N must be at least 4")
-    #     sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
     for i in range(n):
         x = coordenadas(n, i)
         if x is not None and x != []:
             print(x)
+            print('{} and ({}, {})'.format(type(x), len(x), len(x[1])))
