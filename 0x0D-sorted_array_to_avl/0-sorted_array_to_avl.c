8c2b6b2461903c15a9451e24cfee7459ce113de0
RayBar72 <3963@holbertonschool.com>
2022-11-07 20:18:41 -0300

    Backup 221107h2018

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,19 +8,19 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-  size_t mid;
-  avl_t *parent;
+	size_t mid;
+	avl_t *parent;
 
-  if (!array || size < 1)
-    return (NULL);
-  parent = calloc(1, sizeof(avl_t));
-  if (!parent)
-    return (NULL);
-  mid = size / 2;
-  parent->n = array[mid];
-  parent->left = sorted_array_to_avl(array, mid);
-  parent->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
-  parent->left ? parent->left->parent = parent : 0;
-  parent->right ? parent->right->parent = parent : 0;
-  return (parent);
+	if (!array || size < 1)
+		return (NULL);
+	parent = calloc(1, sizeof(avl_t));
+	if (!parent)
+		return (NULL);
+	mid = size / 2;
+	parent->n = array[mid];
+	parent->left = sorted_array_to_avl(array, mid);
+	parent->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
+	parent->left ? parent->left->parent = parent : 0;
+	parent->right ? parent->right->parent = parent : 0;
+	return (parent);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,10 +1,9 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef BINARY_TREES_H_
+#define BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,15 +13,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
