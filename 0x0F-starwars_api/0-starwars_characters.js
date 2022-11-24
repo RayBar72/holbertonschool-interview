2246c7dee71b979b0e10ea2159030a0fc8a84b96
RayBar72 <3963@holbertonschool.com>
2022-11-24 07:33:52 -0300

    Backup 221124

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* Script that prints all character of Starwars movie
+*/
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const c in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[c], (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
+});
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0x0F. Star Wars API
+
+Script that prints all characters of a Star Wars movie
