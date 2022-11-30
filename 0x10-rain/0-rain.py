a63ad92391b27fb039172027e15eb93499260269
RayBar72 <3963@holbertonschool.com>
2022-11-30 07:15:42 -0300

    Back up new version

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -11,38 +11,46 @@ def rain(walls):
     Function that calculates the water reteined by the
     rain
     '''
-    if type(walls) is not list:
-        return 0
+    try:
 
-    for w in walls:
-        if type(w) is not int:
-            return 0
-        if w < 0:
+        if not walls:
             return 0
 
-    if sum(walls) == 0:
+        if type(walls) is not list:
+            return 0
+
+        for w in walls:
+            if type(w) is not int:
+                return 0
+            if w < 0:
+                return 0
+
+        if sum(walls) == 0:
+            return 0
+
+        nums = [x for x in walls if x > 0]
+        if len(nums) == 1:
+            return 0
+
+        indice = []
+        valores = []
+
+        for i, w in enumerate(walls):
+            if w in nums:
+                indice.append(i)
+
+        dist = []
+        minimo = []
+
+        for d in range(len(nums) - 1):
+            m = min(nums[d], nums[d + 1])
+            di = indice[d + 1] - indice[d] - 1
+            minimo.append(m)
+            dist.append(di)
+
+        valores = [x * y for x, y in zip(dist, minimo)]
+
+        return sum(valores)
+
+    except Exception:
         return 0
-
-    nums = [x for x in walls if x > 0]
-    if len(nums) == 1:
-        return 0
-
-    indice = []
-    valores = []
-
-    for i, w in enumerate(walls):
-        if w in nums:
-            indice.append(i)
-
-    dist = []
-    minimo = []
-
-    for d in range(len(nums) - 1):
-        m = min(nums[d], nums[d + 1])
-        di = indice[d + 1] - indice[d] - 1
-        minimo.append(m)
-        dist.append(di)
-
-    valores = [x * y for x, y in zip(dist, minimo)]
-
-    return sum(valores)
