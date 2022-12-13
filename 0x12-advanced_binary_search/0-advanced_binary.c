59458448541217c3454fc8ae0c9abbd9fa3e7c72
RayBar72 <3963@holbertonschool.com>
2022-12-13 08:07:12 -0300

    Backup 221213h0807

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,18 @@
+#include "search.h"
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int *recorre = array, step = 0;
+	size_t counter = 0;
+
+	if (!array)
+		return (-1);
+
+	step = counter / 2;
+
+	while (step)
+	{
+		/* code */
+	}
+	
+}
