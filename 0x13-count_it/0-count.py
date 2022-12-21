09a7b554562f2384cceb453a4e32d70f26ce3068
RayBar72 <3963@holbertonschool.com>
2022-12-21 13:02:44 -0300

    Backup 221221h1302

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,41 +1,79 @@
 #!/usr/bin/python3
+"""Modulus that contains function for counting hotlist in Reddit
+Functions:
+    hot_management: takes a list an counts the number of times that
+                    a word happens
+    impreso: prints the results
+    listados: converts into a dictionary the list of words to be searched
+    recurse: searches the hotlis topics. Recursive function
+    count_words: function that orders the different steps of the process
+"""
 from collections import Counter
 import operator
 import requests
 
 
 def hot_management(lista):
+    """Function that takes a list an counts the number of times of occurence
+
+    Args:
+        lista (list): list returned of recurse
+
+    Returns:
+        dictionary with all the occurrence of each word in the list
+    """
     res = [x.lower() for x in lista]
     res = ' '.join(res)
     res = res.split()
     conteo_res = Counter(res)
     return dict(conteo_res)
 
+
 def impreso(inicial, reddit):
-    # Vaciar valores de reddit en inicial
-    # print(inicial)
-    # print(reddit)
+    """Function that prints the occurrence of searched words
+
+    Args:
+        inicial (list): to be searched
+        reddit (list): to search
+    """
     for k in inicial.keys():
         if k in reddit:
             inicial[k] = reddit[k]
-    # Ordenar inical
-    d = sorted(inicial.items(), key = operator.itemgetter(1), reverse=True)
-    # Impresión
-    # print(d)
+    d = sorted(inicial.items(), key=operator.itemgetter(1), reverse=True)
     for nom in d:
         a, b = nom
         if b != 0:
             print('{}: {}'.format(a, b))
 
+
 def listados(lista):
+    """Function that converts into a dictionary the list of words to be
+       searched
+
+    Args:
+        lista (list): words to be searched
+
+    Returns:
+        dict: dictionary in zero with the words to be searched
+    """
     res = [x.lower() for x in lista]
     res = set(res)
     res = sorted(list(res))
     dito = {k: 0 for k in res}
     return dito
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    '''Getting all posto of hot'''
+    """Function that orders the different steps of the process
+
+    Args:
+        subreddit (str): Topic for title of hot articles
+        hot_list (list, optional): to be search. Defaults to [].
+        after (None, optional): paginator. Defaults to None.
+
+    Returns:
+        list: with all titles of the topic
+    """
     red_url = 'http://reddit.com/r/{}/hot.json'
     header = {'User-agent': 'Prueba:1-top_ten:v1'}
     limit = {'limit': 100}
@@ -44,8 +82,10 @@ def recurse(subreddit, hot_list=[], after=None):
             limit['after'] = after
         else:
             return hot_list
-    resp = requests.get(red_url.format(subreddit), headers=header,
-                        params=limit)
+    resp = requests.get(red_url.format(subreddit),
+                        headers=header,
+                        params=limit,
+                        allow_redirects=False)
     if resp.status_code != 200:
         return None
     data = resp.json().get('data', {})
@@ -55,6 +95,7 @@ def recurse(subreddit, hot_list=[], after=None):
     hot_list = hot_list + [post.get('data', {}).get('title')
                            for post in data.get('children', [])]
     return recurse(subreddit, hot_list, after)
+
 
 def count_words(subreddit, word_list):
     hots_lists = recurse(subreddit)
