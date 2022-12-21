5350178742154210b373987544387daf202428b3
RayBar72 <3963@holbertonschool.com>
2022-12-21 11:45:41 -0300

    Backup 221221

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,7 +4,38 @@ import operator
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
+def hot_management(lista):
+    res = [x.lower() for x in lista]
+    res = ' '.join(res)
+    res = res.split()
+    conteo_res = Counter(res)
+    return dict(conteo_res)
+
+def impreso(inicial, reddit):
+    # Vaciar valores de reddit en inicial
+    # print(inicial)
+    # print(reddit)
+    for k in inicial.keys():
+        if k in reddit:
+            inicial[k] = reddit[k]
+    # Ordenar inical
+    d = sorted(inicial.items(), key = operator.itemgetter(1), reverse=True)
+    # Impresión
+    # print(d)
+    for nom in d:
+        a, b = nom
+        if b != 0:
+            print('{}: {}'.format(a, b))
+
+def listados(lista):
+    res = [x.lower() for x in lista]
+    res = set(res)
+    res = sorted(list(res))
+    dito = {k: 0 for k in res}
+    return dito
+
+def recurse(subreddit, hot_list=[], after=None):
+    '''Getting all posto of hot'''
     red_url = 'http://reddit.com/r/{}/hot.json'
     header = {'User-agent': 'Prueba:1-top_ten:v1'}
     limit = {'limit': 100}
@@ -21,20 +52,15 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
     after = data.get('after', 'STOP')
     if not after:
         after = 'STOP'
-    hot_list = hot_list + [post.get('data', {}).get('title') for post in data.get('children', [])]
-    hot = count_words(subreddit, word_list, hot_list, after)
-    hot = [x.lower() for x in hot]
-    word_list = [x.lower() for x in word_list]
-    word_list = [(word_list)].sort()
-    pre_imp = {}
-    if type(word_list) is list:
-        pre_imp = {k: v for k, v in zip(word_list, [0] * len(word_list))}
-    cuenta = Counter(hot)
-    for k, _ in pre_imp.items():
-        if cuenta.get(k):
-            pre_imp[k] = cuenta.get(k)
-    impre = sorted(pre_imp.items(), key=operator.itemgetter(1), reverse=True)
-    for i in impre:
-        a, b = i
-        if b > 0:
-            print('{}: {}'.format(a, b))
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return recurse(subreddit, hot_list, after)
+
+def count_words(subreddit, word_list):
+    hots_lists = recurse(subreddit)
+    if hots_lists:
+        initial = listados(word_list)
+        hot_dict = hot_management(hots_lists)
+        impreso(initial, hot_dict)
+    else:
+        return None
