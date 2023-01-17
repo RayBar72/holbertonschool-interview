368e0b37e95297ee16ece969793af23074edf95a
RayBar72 <3963@holbertonschool.com>
2023-01-17 10:36:15 -0300

    First

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module 0x16. Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    largo = len(matrix)
+    largo
+
+    copia = []
+    for i in range(largo):
+        r_copia = []
+        for j in range(largo):
+            r_copia.append(matrix[largo - 1 - j][i])
+        copia.append(r_copia)
+    copia
+
+    for i in range(largo):
+        matrix[i] = copia[i]
+    matrix
