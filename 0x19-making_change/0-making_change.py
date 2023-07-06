98a810d2cac4fb5dce791d3d4e67b18ce04a8c6d
RayBar72 <3963@holbertonschool.com>
2023-07-06 09:39:01 -0300

    Traying with greedy

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -15,19 +15,20 @@ def makeChange(coins, total):
         int: -1 if no solution, 0 if total equal or less
         than 0 or fewest number of coins
     """
-    dp = [float('inf')] * (total + 1)
 
     if total <= 0:
         return 0
 
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
+    coins.sort(reverse=True)
+    
+    count = 0
+    
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+    
+    if total != 0:
         return -1
-
-    return dp[total]
+    
+    return count
