956ed3160fcb1da0273e528bf94b2f4762c43a79
RayBar72 <3963@holbertonschool.com>
2023-07-06 10:00:25 -0300

    Resolving pycodestyle

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,33 +1,17 @@
-#!/usr/bin/python3
-"""
-0-making_chage.py
-"""
-
-
 def makeChange(coins, total):
-    """Function that calculates making change problem
+    dp = [float('inf')] * (total + 1)
 
-    Args:
-        coins (list): coins available
-        total (int): amount to use
-
-    Returns:
-        int: -1 if no solution, 0 if total equal or less
-        than 0 or fewest number of coins
-    """
     if total <= 0:
         return 0
 
-    coins = sorted(coins, reverse=True)
+    dp[0] = 0
 
-    count = 0
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    for coin in coins:
-        while total >= coin:
-            total -= coin
-            count += 1
-
-    if total != 0:
+    if dp[total] == float('inf'):
         return -1
 
-    return count
+    return dp[total]
