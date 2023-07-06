14ab9322a47864463e5bcd71dff7bcefbfd12bbb
RayBar72 <3963@holbertonschool.com>
2023-07-06 10:25:30 -0300

    Resolving pycodestyle

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -11,44 +11,23 @@ def makeChange_sub(coins, total, solutions):
     Args:
         coins (list): coins available
         total (int): amount to use
-        solutions (list): posible solutions
     Returns:
         int: -1 if no solution, 0 if total equal or less
         than 0 or fewest number of coins
     """
-    if total < 0:
+    if total <= 0:
         return -1
-    if total == 0:
-        return 0
 
-    if solutions[total - 1] != 0:
-        return solutions[total - 1]
+    coins = sorted(coins, reverse=True)
 
-    optimal_solution = float('inf')
+    count = 0
 
     for coin in coins:
-        best_solution_for_coin = makeChange_sub(coins, total - coin, solutions)
-        if 0 <= best_solution_for_coin < optimal_solution:
-            optimal_solution = best_solution_for_coin + 1
+        while total >= coin:
+            total -= coin
+            count += 1
 
-    if optimal_solution == float('inf'):
-        solutions[total - 1] = -1
-    else:
-        solutions[total - 1] = optimal_solution
+    if total != 0:
+        return -1
 
-    return solutions[total - 1]
-
-
-def makeChange(coins, total):
-    """Function that calculates making change problem
-    Args:
-        coins (list): coins available
-        total (int): amount to use
-    Returns:
-        int: -1 if no solution, 0 if total equal or less
-        than 0 or fewest number of coins
-    """
-    if total < 1:
-        return 0
-    else:
-        return makeChange_sub(coins, total, [0] * total)
+    return count
