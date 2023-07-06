dbd92779316e692250c47ce3341c8bbc662f4bac
RayBar72 <3963@holbertonschool.com>
2023-07-06 09:40:55 -0300

    Resolving pycodestyle

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -20,15 +20,15 @@ def makeChange(coins, total):
         return 0
 
     coins.sort(reverse=True)
-    
+
     count = 0
-    
+
     for coin in coins:
         while total >= coin:
             total -= coin
             count += 1
-    
+
     if total != 0:
         return -1
-    
+
     return count
