8dbace6de7550364878999eeee4c29f2d635848e
RayBar72 <3963@holbertonschool.com>
2023-07-06 09:22:15 -0300

    Changing from recursive to loop

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,42 +4,6 @@
 """
 
 
-def makeChange_sub(coins, total, solutions):
-    """Function that calculates in recursive way
-        a dynamic solution for making change problem
-
-    Args:
-        coins (list): coins available
-        total (int): amount to use
-        solutions (list): posible solutions
-
-    Returns:
-        int: -1 if no solution, 0 if total equal or less
-        than 0 or fewest number of coins
-    """
-    if total < 0:
-        return -1
-    if total == 0:
-        return 0
-
-    if solutions[total - 1] != 0:
-        return solutions[total - 1]
-
-    optimal_solution = float('inf')
-
-    for coin in coins:
-        best_solution_for_coin = makeChange_sub(coins, total - coin, solutions)
-        if 0 <= best_solution_for_coin < optimal_solution:
-            optimal_solution = best_solution_for_coin + 1
-
-    if optimal_solution == float('inf'):
-        solutions[total - 1] = -1
-    else:
-        solutions[total - 1] = optimal_solution
-
-    return solutions[total - 1]
-
-
 def makeChange(coins, total):
     """Function that calculates making change problem
 
@@ -51,7 +15,19 @@ def makeChange(coins, total):
         int: -1 if no solution, 0 if total equal or less
         than 0 or fewest number of coins
     """
-    if total < 1:
+    dp = [float('inf')] * (total + 1)
+
+    if total <= 0:
         return 0
-    else:
-        return makeChange_sub(coins, total, [0] * total)
+
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
