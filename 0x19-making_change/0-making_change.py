baf05626b157fcbf3f9be690a91e570338056164
RayBar72 <3963@holbertonschool.com>
2023-07-07 01:35:54 +0000

    Pycode

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
+'''
 0-making_chage.py
 Modulus that works making chang
-"""
+'''
 
 
 def makeChange(coins, total):
-    """Function that calculates in recursive way
+    '''Function that calculates in recursive way
         a dynamic solution for making change problem
     Args:
         coins (list): coins available
@@ -14,7 +14,7 @@ def makeChange(coins, total):
     Returns:
         int: -1 if no solution, 0 if total equal or less
         than 0 or fewest number of coins
-    """
+    '''
     if total <= 0:
         return 0
 
