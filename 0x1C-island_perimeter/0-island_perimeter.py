3edf96fb1bb2237ca2a63a1c9b0fa65a7a1496f6
RayBar72 <3963@holbertonschool.com>
2023-07-27 22:34:37 +0000

    Border case

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -15,41 +15,28 @@ def island_perimeter(grid):
     """
     if type(grid) is not list and type(grid[0]) is not list:
         return 0
-    for row in grid:
-        for x in row:
-            if x != 0 and x != 1:
-                return 0
     retorno = []
     for i, row in enumerate(grid):
-        renglon = []
         for j, g in enumerate(row):
             numero = 0
             if g == 1:
-                numero = 0
+                numero = 4
             # Arriba
-                try:
-                    if grid[i - 1][j] == 0:
-                        numero += 1
-                except Exception as e:
-                    pass
+                if i != 0:
+                    if grid[i - 1][j] == 1:
+                        numero -= 1
             # Abajo
-                try:
-                    if grid[i + 1][j] == 0:
-                        numero += 1
-                except Exception as e:
-                    pass
+                if i != len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        numero -= 1
             # Izquierda
-                try:
-                    if grid[i][j - 1] == 0:
-                        numero += 1
-                except Exception as e:
-                    pass
+                if j != 0:
+                    if grid[i][j - 1] == 1:
+                        numero -= 1
             # Derecha
-                try:
-                    if grid[i][j + 1] == 0:
-                        numero += 1
-                except Exception as e:
-                    pass
-            renglon.append(numero)
-        retorno.append(renglon)
-    return sum([sum(x) for x in retorno])
+                if j != len(grid[0]) - 1:
+                    if grid[i][j + 1] == 1:
+                            numero -= 1
+                retorno.append(numero)
+    # return sum(retorno)
+    return retorno
