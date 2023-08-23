0086e2e712a612dc503c31fcc85a070c63a83a6f
RayBar72 <3963@holbertonschool.com>
2023-08-23 21:04:59 +0000

    Second try

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -10,13 +10,9 @@
 int regex_match(char const *str, char const *pattern)
 {
 	if (!*str)
-		return (!*pattern || (pattern[1] == '*' \
-				&& regex_match(str, pattern + 2)));
+		return (!*pattern || (pattern[1] == '*' && regex_match(str, pattern + 2)));
 	if (pattern[1] == '*')
-		return ((*pattern == '.' || *str == *pattern) \
-				? (regex_match(str + 1, pattern) || \
-				regex_match(str, pattern + 2)) \
-				: (regex_match(str, pattern + 2)));
-	return ((*pattern == '.' || *pattern == *str) && \
-			regex_match(str + 1, pattern + 1));
+		return ((*pattern == '.' || *str == *pattern) ? (regex_match(str + 1, pattern) || \
+				regex_match(str, pattern + 2)): (regex_match(str, pattern + 2)));
+	return ((*pattern == '.' || *pattern == *str) && regex_match(str + 1, pattern + 1));
 }
