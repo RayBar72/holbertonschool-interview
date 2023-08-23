8c2a2b2cc253dc73443a349f6a9a323ece6ae347
RayBar72 <3963@holbertonschool.com>
2023-08-23 22:18:29 +0000

    Changing the var names for betty

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -12,7 +12,7 @@ int regex_match(char const *s, char const *p)
 	if (!*s)
 		return (!*p || (p[1] == '*' && regex_match(s, p + 2)));
 	if (p[1] == '*')
-		return ((*p == '.' || *s == *p) ? (regex_match(s + 1, p) || 
+		return ((*p == '.' || *s == *p) ? (regex_match(s + 1, p) ||
 				regex_match(s, p + 2)) : (regex_match(s, p + 2)));
 	return ((*p == '.' || *p == *s) && regex_match(s + 1, p + 1));
 }
